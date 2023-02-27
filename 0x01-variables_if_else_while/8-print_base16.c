d14a20520f5e57bf5afb030e1f472c1268ce7fab
brackton <bracktongitonga254@gmail.com>
2023-02-27 13:26:25 -0800

    TASK 8 and 9

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 
 /**
- * main - prints the numbers in base 16
- * Return: Always 0
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
+
 int main(void)
+
 {
-	int num;
-	char la;
 
-	for (num = 0; num < 10; num++)
-		putchar((num % 10) + '0');
+	int d;
+	char low;
 
-	for (num = 'a'; num <= 'f'; la++)
-		putchar(la);
+	for (d = '0'; d <= '9'; d++)
+	putchar(d);
 
+	for (low = 'a'; low <= 'f'; low++)
+	putchar(low);
 	putchar('\n');
 
 	return (0);
