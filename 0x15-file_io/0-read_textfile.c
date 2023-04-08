b1172649c489feabda408e24e70e70174a41b2f2
brackton <bracktongitonga254@gmail.com>
2023-04-08 11:31:00 -0700

    Task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,31 +1,34 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
-  * read_textfile - ...
-  * @filename: The source file
-  * @letters: Number of letters to reads and prints
-  *
-  * Return: ...
-  */
+ * read_textfile - reads text from a file and prints it
+ * @filename: name of file to read
+ * @letters: nummber of bytes to read
+ *
+ * Return: number bytes read/printed
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, readed;
-	char *buff = malloc(sizeof(char *) * letters);
-
-	if (!buff)
-		return (0);
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
 
 	if (!filename)
 		return (0);
 
-	fd = open(filename, O_RDONLY, 0600);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
 
-	readed = read(fd, buff, letters);
-	write(STDOUT_FILENO, buff, readed);
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 
-	free(buff);
 	close(fd);
-	return (readed);
-}
+
+	free(buf);
+
+	return (nwr);
+}}
