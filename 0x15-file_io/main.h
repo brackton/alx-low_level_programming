7613455cfad6bf76c614584a2317848b0395571b
brackton <bracktongitonga254@gmail.com>
2023-04-08 11:24:39 -0700

    Task 0

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,17 +1,14 @@
-#ifndef _FILE_IO_
-#define _FILE_IO_
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <sys/stat.h>
 #include <sys/types.h>
-#include <sys/uio.h>
-#include <fcntl.h>
+#include <sys/stat.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
-int _strlen(char *s);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void copy_file(const char *src, const char *dest);
 
 #endif
